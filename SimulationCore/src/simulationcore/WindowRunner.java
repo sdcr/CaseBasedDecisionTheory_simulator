183429c6035d0e3250cfb242fb950c261057ab3a
Stephan da Costa Ribeiro <sdcr@gmx.de>
2013-01-31 23:34:13 +0100

    Main window layout with main Composites created

--- SimulationCore/src/simulationcore/WindowRunner.java
+++ SimulationCore/src/simulationcore/WindowRunner.java
@@ -1,9 +1,15 @@
 package simulationcore;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.osgi.framework.Bundle;
@@ -23,7 +29,29 @@ public class WindowRunner {
 		
 		Display display = new Display ();
 		Shell shell = new Shell(display);
-//		shell.setLayout(new FillLayout());
+		
+		GridLayout windowLayout = new GridLayout(2,false);
+		shell.setLayout(windowLayout);
+		
+		//create 
+		ScrolledComposite pluginsBar = new ScrolledComposite(shell, SWT.PUSH);
+		Color simulationPluginsBackgroundColor = new Color(display, 0, 0, 255);
+		pluginsBar.setBackground(simulationPluginsBackgroundColor);
+		pluginsBar.setExpandVertical(true);
+		GridData pluginsBarGridData = new GridData();
+		pluginsBarGridData.verticalAlignment = GridData.FILL;
+		pluginsBarGridData.grabExcessVerticalSpace = true;
+		pluginsBar.setLayoutData(pluginsBarGridData);
+		
+		//create the mainPane Composite
+		Composite mainPane = new Composite(shell, SWT.PUSH);
+		Color mainPaneColor = new Color(display, 200, 200, 200);
+		mainPane.setBackground(mainPaneColor);
+		GridData mainPaneGridData = new GridData();
+		mainPaneGridData.verticalAlignment = GridData.FILL;
+		mainPaneGridData.horizontalAlignment = GridData.FILL;
+		mainPaneGridData.grabExcessHorizontalSpace = true;
+		mainPane.setLayoutData(mainPaneGridData);
 		
 		shell.addMouseListener(new MouseListener() {
 			
