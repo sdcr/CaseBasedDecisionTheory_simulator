1caff8ebb281e34191f003175e36031e8f47687a
Stephan da Costa Ribeiro <sdcr@gmx.de>
2013-03-17 16:00:55 +0100

    Added a plugin which contains the processing core library.

--- CBDT/src/cbdt/view/analysis/AnalysisPageFactory.java
+++ CBDT/src/cbdt/view/analysis/AnalysisPageFactory.java
@@ -1,15 +1,26 @@
 package cbdt.view.analysis;
 
+import java.awt.Frame;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import cbdt.view.analysis.tree.TreePApplet;
 
 import simulation.extensionpoint.simulationplugin.definition.AbstractPluginPageWrapper;
 
 public class AnalysisPageFactory extends AbstractPluginPageWrapper{
 
 	
+	private static final int TREE_ANIMATION_HEIGHT = 800;
+	private static final int TREE_ANIMATION_WIDTH = 800;
+
 	@Override
 	public String getName() {
 		return "Ergebnis-Analyse";
@@ -24,7 +35,36 @@ public class AnalysisPageFactory extends AbstractPluginPageWrapper{
 		parameterComposite.setLayout(rowLayout);
 		Label l = new Label(parameterComposite, SWT.NONE);
 		l.setText("Analyse");
+		
+		showTreeAnimation(parent);
+		
 		return parameterComposite;
+	}
+
+	private void showTreeAnimation(Composite parent) {
+		Display display = parent.getDisplay();
+		Shell shell = new Shell(display);
+		shell.setLayout(new RowLayout());
+		shell.setSize(TREE_ANIMATION_WIDTH + 20, TREE_ANIMATION_HEIGHT + 20);
+		
+		Composite appletComposite = new Composite(shell, SWT.EMBEDDED | SWT.NO_BACKGROUND);
+		
+		RowData rData = new RowData();
+		rData.width = TREE_ANIMATION_WIDTH;
+		rData.height = TREE_ANIMATION_HEIGHT;
+		appletComposite.setLayoutData(rData);
+		
+		Frame frame = SWT_AWT.new_Frame(appletComposite);
+		
+		TreePApplet treePApplet = new TreePApplet();
+		frame.add(treePApplet);
+		treePApplet.init();
+		shell.open();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+//		treePApplet.stop();
+		shell.dispose ();
 	}
 
 }
--- CBDT/src/cbdt/view/analysis/tree/TreePApplet.java
+++ CBDT/src/cbdt/view/analysis/tree/TreePApplet.java
@@ -1,5 +1,42 @@
 package cbdt.view.analysis.tree;
 
-public class TreePApplet {
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
+import processing.core.PApplet;
+
+public class TreePApplet extends PApplet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void setup() {
+		size(500, 500);
+		background(255);
+
+		addMouseWheelListener(new MouseWheelListener() {
+			public void mouseWheelMoved(MouseWheelEvent mwe) {
+				mouseWheel(mwe.getWheelRotation());
+			}
+		});
+
+	}
+
+	protected void mouseWheel(int wheelRotation) {
+		// TODO Auto-generated method stub
+		System.out.println(wheelRotation);
+	}
+
+	public void draw() {
+		ellipseMode(CENTER);
+		for (int i = 0; i < 10; i++)
+			ellipse(300, i * 200, 100, 100);
+
+		// stroke(255);
+		// if (mousePressed) {
+		// line(mouseX,mouseY,pmouseX,pmouseY);
+		// }
+	}
 }
