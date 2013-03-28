6e5a075c3810d398ddda0ce4baa1c3f695c96327
Stephan da Costa Ribeiro <sdcr@gmx.de>
2013-03-28 22:51:07 +0100

    Added a provider for indexes where the aspiration level should be increased.

---
+++ CBDT/src/cbdt/control/simulation/algorithm/IndexOfAspirationLevelIncrementStageProvider.java
@@ -0,0 +1,8 @@
+package cbdt.control.simulation.algorithm;
+
+public class IndexOfAspirationLevelIncrementStageProvider {
+
+	public boolean isStageToIncreaseAspirationLevel(int indexOfStage){
+		return false;
+	}
+}
