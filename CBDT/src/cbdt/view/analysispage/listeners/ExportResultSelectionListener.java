05b29cdcb9c4091fb7f63a18be87ed2e24ee2959
Stephan da Costa Ribeiro <sdcr@gmx.de>
2013-12-09 20:39:18 +0900

    Some more comments.

--- CBDT/src/cbdt/view/analysispage/listeners/ExportResultSelectionListener.java
+++ CBDT/src/cbdt/view/analysispage/listeners/ExportResultSelectionListener.java
@@ -5,13 +5,26 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Shell;
 
 import cbdt.control.analysispage.AnalysisPageController;
+import cbdt.model.result.Result;
 import cbdt.view.menu.listeners.OpenFileDialogSelectionListener;
 
-public class ExportResultSelectionListener extends OpenFileDialogSelectionListener {
+/**
+ * This class is used to listen for a {@link SelectionEvent} to export the
+ * {@link Result}s into a file.
+ * 
+ * @author Stephan da Costa Ribeiro
+ * 
+ */
+public class ExportResultSelectionListener extends
+		OpenFileDialogSelectionListener {
 
+	/**
+	 * The {@link AnalysisPageController}.
+	 */
 	private AnalysisPageController controller;
 
-	public ExportResultSelectionListener(Shell shell, AnalysisPageController controller) {
+	public ExportResultSelectionListener(Shell shell,
+			AnalysisPageController controller) {
 		super(shell);
 		this.controller = controller;
 	}
@@ -19,8 +32,9 @@ public class ExportResultSelectionListener extends OpenFileDialogSelectionListen
 	@Override
 	public void widgetSelected(SelectionEvent e) {
 		String[] filterExt = { "*.csv", "*.*" };
-		String filePath = getFilepathFromDialog(SWT.SAVE, "Export simulation results", filterExt);
-		if(filePath != null)
+		String filePath = getFilepathFromDialog(SWT.SAVE,
+				"Export simulation results", filterExt);
+		if (filePath != null)
 			controller.exportResults(filePath);
 	}
 
--- CBDT/src/cbdt/view/analysispage/listeners/ShowTreeSelectionListener.java
+++ CBDT/src/cbdt/view/analysispage/listeners/ShowTreeSelectionListener.java
@@ -16,16 +16,26 @@ import cbdt.model.config.engine.DFSkeepTreeEngineConfig;
 import cbdt.model.result.Result;
 import cbdt.view.analysispage.tree.TreePApplet;
 
+//TODO commenting, maybe clean up
+/**
+ * This class inherits from {@link SelectionListener} and is used to listen for
+ * the {@link SelectionEvent} to display the tree structure from the computed
+ * simulation {@link Result}s.
+ * 
+ * @author Stephan da Costa Ribeiro
+ * 
+ */
 public class ShowTreeSelectionListener implements SelectionListener {
 
 	public static final int TREE_ANIMATION_HEIGHT = 800;
 	public static final int TREE_ANIMATION_WIDTH = 800;
-	
+
 	private Result simulationResult;
 	private Composite parent;
 	private DFSkeepTreeEngineConfig config;
 
-	public ShowTreeSelectionListener(Result simulationResult, DFSkeepTreeEngineConfig config, Composite parent) {
+	public ShowTreeSelectionListener(Result simulationResult,
+			DFSkeepTreeEngineConfig config, Composite parent) {
 		this.simulationResult = simulationResult;
 		this.config = config;
 		this.parent = parent;
@@ -33,45 +43,52 @@ public class ShowTreeSelectionListener implements SelectionListener {
 
 	@Override
 	public void widgetSelected(SelectionEvent e) {
-		System.out.println("ShowTreeSelectionListener.widgetSelected(): after entering method");
+		System.out
+				.println("ShowTreeSelectionListener.widgetSelected(): after entering method");
 		showTreeAnimation();
-		System.out.println("ShowTreeSelectionListener.widgetSelected(): before leaving method");
+		System.out
+				.println("ShowTreeSelectionListener.widgetSelected(): before leaving method");
 	}
 
 	@Override
 	public void widgetDefaultSelected(SelectionEvent e) {
 	}
-	
+
 	private void showTreeAnimation() {
 		Display display = parent.getDisplay();
 		Shell shell = new Shell(display);
 		shell.setLayout(new RowLayout());
 		shell.setSize(TREE_ANIMATION_WIDTH + 30, TREE_ANIMATION_HEIGHT + 50);
-		
-		Composite appletComposite = new Composite(shell, SWT.EMBEDDED | SWT.NO_BACKGROUND);
-		
+
+		Composite appletComposite = new Composite(shell, SWT.EMBEDDED
+				| SWT.NO_BACKGROUND);
+
 		RowData rData = new RowData();
 		rData.width = TREE_ANIMATION_WIDTH;
 		rData.height = TREE_ANIMATION_HEIGHT;
 		appletComposite.setLayoutData(rData);
-		
+
 		Frame frame = SWT_AWT.new_Frame(appletComposite);
-		
-		System.out.println("ShowTreeSelectionListener.showTreeAnimation(): before treePApplet creation");
+
+		System.out
+				.println("ShowTreeSelectionListener.showTreeAnimation(): before treePApplet creation");
 		TreePApplet treePApplet = new TreePApplet();
-		System.out.println("ShowTreeSelectionListener.showTreeAnimation(): after treePApplet creation");
-		
+		System.out
+				.println("ShowTreeSelectionListener.showTreeAnimation(): after treePApplet creation");
+
 		frame.add(treePApplet);
 		treePApplet.setTreeModel(simulationResult.getRootNode(), config);
 
-		System.out.println("ShowTreeSelectionListener.showTreeAnimation(): before treePApplet.init() call");
+		System.out
+				.println("ShowTreeSelectionListener.showTreeAnimation(): before treePApplet.init() call");
 		treePApplet.init();
-		System.out.println("ShowTreeSelectionListener.showTreeAnimation(): after treePApplet.init() call");
+		System.out
+				.println("ShowTreeSelectionListener.showTreeAnimation(): after treePApplet.init() call");
 
 		shell.open();
-		
-		while(!shell.isDisposed()) {
-			if (!display.readAndDispatch()) 
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
 				display.sleep();
 		}
 		treePApplet.stop();
